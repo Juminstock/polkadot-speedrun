c69f995ce05f13e6c667d3f0f9ff86186b393665
Carlos Rodríguez <juminstock@gmail.com>
2024-07-22 22:01:02 -0400

    Update: create new project with next

--- docs/intro.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 1
----
-
-# Getting Started
-
-Working on this...
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Hello, Polkadot SpeedRun!</h1>
+  );
+}
